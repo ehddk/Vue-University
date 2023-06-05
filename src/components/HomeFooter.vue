895b54ebbb29c0e69f98ba0c7e1b3f252d191e17
ehddk <86900169+ehddk@users.noreply.github.com>
2023-06-06 08:49:45 +0900

    Add files via upload

--- src/components/HomeFooter.vue
+++ src/components/HomeFooter.vue
@@ -1,23 +1,39 @@
 <template>
-  <div class="foot"></div>
+	<div class="foot">
+		<div class="foot_info">
+			<div class="foot_link">
+				<ul style="display: flex; padding: 20px 0 0 0px">
+					<h3>커모아</h3>
+					<li>개인정보처리방침</li>
+					<li>문의하기</li>
+					<li>공지사항</li>
+					<li>서비스이용약관</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name:"homefooter"
-}
+	name: 'homefooter',
+};
 </script>
 
 <style>
-.foot{
-  background:beige;
-  /* position: fixed; */
-bottom: 0;
-left: 0;
-right: 0;
-min-width:1100px;
-height:200px;
-
+.foot {
+	background: rgb(198, 211, 219);
+	/* position: fixed; */
+	bottom: 0;
+	left: 0;
+	right: 0;
+	/* width: 1100px; */
+	height: 100px;
+	display: flex;
+}
+.foot_info {
+	align-content: center;
+	margin: 0 auto;
 }
 /* h1{
 
@@ -26,4 +42,7 @@ height:200px;
 background:navy;
  
 } */
+.foot li {
+	margin-left: 20px;
+}
 </style>
---
+++ src/components/UnivModal.vue
@@ -0,0 +1,109 @@
+<template>
+	<!----모달창 띄우기-->
+	<section v-if="isOpenModal" class="modal">
+		<div id="content" style="background: white">
+			<div class="content_header" style="padding: 50px">
+				<h2>대학 비교</h2>
+			</div>
+			<div class="univ_body" style="padding: 50px 0 0 100px">
+				<div class="univ_content">
+					<table class="univ_table">
+						<colgroup>
+							<col style="width: 200px" />
+							<col style="width: 200px" />
+							<col style="width: 200px" />
+							<col style="width: 200px" />
+						</colgroup>
+						<thead style="background: lightgray">
+							<tr>
+								<th scope="col" style="padding: 20px">카테고리</th>
+								<th scope="col">대학교명</th>
+								<th scope="col">대학교명</th>
+								<th scope="col">대학교명</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td style="padding: 30px">전형 유형</td>
+								<td>{{ item }}</td>
+							</tr>
+							<tr>
+								<td style="padding: 30px">전체 수</td>
+							</tr>
+							<tr>
+								<td style="padding: 30px">주간 / 야간</td>
+							</tr>
+							<tr>
+								<td style="padding: 30px">남녀 성비</td>
+							</tr>
+							<tr>
+								<td style="padding: 30px">전공심화 여부</td>
+							</tr>
+						</tbody>
+					</table>
+					<button class="close_btn" @click.stop="closeModal">닫기</button>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	props: {
+		isOpenModal: {
+			type: Boolean,
+			required: true,
+		},
+	},
+
+	methods: {
+		closeModal() {
+			this.$emit('closeModal');
+		},
+		// close(event) {
+
+		// 	if (event.target.classList.contains('modal')) {
+		// 		this.$emit('closeModal');
+		// 	}
+		// },
+	},
+};
+</script>
+
+<style>
+body {
+	background: #f4f5f9;
+}
+.content_header {
+	padding: 15px;
+}
+.univ_body {
+	margin-top: 50px;
+	height: auto;
+}
+.univ_table {
+	border: 1px solid #ababab;
+}
+.univ_table th {
+	border-bottom: 1px solid #ababab;
+}
+colgroup {
+	display: table-column-group;
+}
+col {
+	display: table-column;
+}
+.modal {
+	display: flex;
+	align-items: center;
+	width: 100%;
+	height: 100%;
+	top: 0;
+	left: 0;
+	z-index: 9999;
+	background-color: rgba(0, 0, 0, 0.432);
+	position: fixed;
+	padding: 20px;
+}
+</style>
